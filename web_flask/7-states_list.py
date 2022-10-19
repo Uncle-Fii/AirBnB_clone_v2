564b302c42d81c1a0e7e9217bd4b6b85b3a2c0b0
Uncle-Fii <fkankrumah@gmail.com>
2022-10-19 16:07:52 -0700

    updates files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+Starting my first Flask web application
 """
-
-from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+from os import environ
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def close_db(error):
+    """
+    Remove the SQLAlchemy session
+    """
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+def list_states():
+    """
+    A route that displays cities in our database in html
+    """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
-    storage.close()
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000, debug=True)
